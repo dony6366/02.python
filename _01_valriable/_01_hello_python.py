6424f956abefdd2c26be5dab25ae75e2e55c966d
dony6366 <rudmony@gmail.com>
2026-05-29 16:18:52 +0900

    20260529_변수

---
+++ _01_valriable/_01_hello_python.py
@@ -0,0 +1,22 @@
+# 주석(Comment) : 코드 설명 작성
+# - 이 주석은 파이썬 인터프리터가 해석하지 않고 넘어감(무시)
+from debugpy._vendored.pydevd._pydevd_bundle import pydevd_runpy
+
+# 한 줄 주석 (window : ctrl +/ , mac : Cmd +/)
+print("hello python") # 문자열 "", ''
+print('안녕 파이썬')
+print(123) # 정수
+print(123.456) #실수
+
+# 여러 줄 문자열 작성
+print("""파이썬
+테스트
+중입니다~~ 
+반가워요~
+잘 지내봅시다~~""")
+
+# 여러 줄 주석
+"""
+그냥 이렇게 작성해도 주석으로 되지롱~
+히히히히
+하하하 출력 안되지롱"""

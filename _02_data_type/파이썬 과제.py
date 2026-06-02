3de5a45aca9b036c43f2aeb5b30ffd358c3e3f24
dony6366 <rudmony@gmail.com>
2026-06-02 14:20:49 +0900

    20260602_if문

---
+++ _02_data_type/파이썬 과제.py
@@ -0,0 +1,42 @@
+# 아래 설명에 맞게 변수를 선언하고 값을 대입하세요.
+from pygments.styles.dracula import orange
+
+#1. `age`라는 이름의 변수를 선언하고, **당신의 나이**를 숫자로 저장하세요.
+# 1. `name`이라는 이름의 변수를 선언하고, **당신의 이름**을 문자열로 저장하세요.
+age = 33
+name = "최경돈"
+
+#다음 조건에 맞게 변수를 작성해보세요.
+
+#1. `height`라는 변수에 **키**를 숫자형으로 저장하세요.
+#1. `dream`이라는 변수에 **당신의 목표**을 문자열로 저장하세요.
+
+height = 185
+dream = "부자 되기"
+
+#1. `city`라는 변수에 **당신이 살고 있는 도시 이름**을 문자열로 저장하세요.
+#2. `my_city`라는 변수에 `city` 변수의 값을 대입하세요.
+#3. 두 변수를 각각 `print()`로 출력해보세요.
+city = "서둘시"
+my_city = city
+print(city)
+print(my_city)
+
+#1. **사과의 개수**를 나타내는 변수를 `apple_count`로 선언하고 숫자 `10`을 저장하세요.
+#2    1. **오렌지의 개수**를 나타내는 변수를 `orange_count`로 선언하고 숫자 `20`을 저장하세요.
+#2. 두 변수를 출력해보세요.
+
+apple_count = 10
+orange_count = 20
+print(apple_count)
+print(orange_count)
+
+#1. 변수 이름은 **스네이크 케이스**를 따릅니다.
+#2. `first_name`에 **당신의 이름의 첫 글자**를 문자열로 저장하세요.
+#3. `last_name`에 **당신의 성**을 문자열로 저장하세요.
+#4. `full_name` 변수에 `first_name`과 `last_name`을 이어붙여서 저장하세요.
+#5. `print()`를 사용해 `full_name`을 출력해보세요.
+first_name = '최'
+last_name ='경돈'
+full_name = first_name + ' ' + last_name
+print(full_name)

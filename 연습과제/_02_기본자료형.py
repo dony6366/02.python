38aa54f9963607303acf1025c0d930daa35976e1
dony6366 <rudmony@gmail.com>
2026-06-04 18:47:21 +0900

    20260604_과제연습

--- 연습과제/_02_기본자료형.py
+++ 연습과제/_02_기본자료형.py
@@ -1,20 +1,59 @@
-#1. `num1`에 10을 저장하고, `num2`에 5를 저장합니다.
-#2. 두 변수를 더한 결과를 `sum_result`에 저장하고 출력하세요.
-num1 = 18
+# 1. `num1`에 10을 저장하고, `num2`에 5를 저장합니다.
+# 2. 두 변수를 더한 결과를 `sum_result`에 저장하고 출력하세요.
+num1 = 10
 num2 = 5
 sum_result = num1 + num2
+print(sum_result)
 
-#1. `height`라는 변수에 **키**를 숫자형으로 저장하세요.
-#2. `dream`이라는 변수에 **당신의 목표**을 문자열로 저장하세요.
-height = 185
-dream = "취업하기"
+# 1. `x`에 7.5를 저장하고, `y`에 2를 저장합니다.
+# 2. `x`를 `y`로 나눈 결과를 `result`에 저장하고 출력하세요.
+x = 7.5
+y = 2
+result = x/y
+print(result)
 
-#1. `city`라는 변수에 **당신이 살고 있는 도시 이름**을 문자열로 저장하세요.
-#2. `my_city`라는 변수에 `city` 변수의 값을 대입하세요.
-#3. 두 변수를 각각 `print()`로 출력해보세요.
-city = '서울'
-my_city = city
-print(city)
-print(my_city)
+# 1. `is_student`라는 논리형 변수를 선언하고 **True**를 저장합니다.
+# 2. 이 변수를 출력하고, 자료형도 함께 출력하세요.
+is_student = True
+print(is_student)
+print(type(is_student))
+
+# 1. `num1`에 15를 저장하고, `num2`에 4를 저장합니다.
+# 2. 두 수의 **나머지**와 **몫**을 각각 구하여 출력하세요.
+num1 = 15
+num2 = 4
+print(num1 % num2)
+print(num1 // num2)
+
+# 1. `greeting_message` 변수에 문자열 `"Hello"`를 저장합니다.
+# 2. `greeting_target` 변수에 `"World"`를 저장합니다.
+# 3. 두 문자열을 이어 붙여 `"HelloWorld"`를 출력하세요.
+greeting_message = 'Hello'
+greeting_target = 'world'
+print(greeting_message + greeting_target)
+
+# 1. `base`에 숫자 2를 저장하고, `exponent`에 숫자 5를 저장합니다.
+# 2. `base`를 `exponent`만큼 제곱한 결과를 출력하세요.
+
+base = 2
+exponent = 5
+answer = base ** exponent
+print(answer)
+
+
+# 1. `number`라는 변수에 숫자 3을 저장합니다.
+# 2. 이 숫자가 **짝수인지 홀수인지**를 확인하세요.
+#   “확인”만 하는 것이므로 조건문은 사용하지 않습니다.
+
+number = 3
+print(number % 2) #홀수
+
+# 1. `age`에 당신의 나이, `city`에 당신의 거주지를 저장합니다.
+# 2. f-string을 사용하여 다음 문장과 같은 형태로 출력하세요.
+#     - `"나는 25살이고, Seoul에 살고 있습니다."`
+age = 33
+city = 'seoul'
+print(f'나는 {age}살이고, {city}에 살고 있습니다.')
+
 
 

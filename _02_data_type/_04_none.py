36a1883389504e31b5c1f49eab3f0a8efd2b3d9d
dony6366 <rudmony@gmail.com>
2026-06-25 17:49:23 +0900

    20260625

--- _02_data_type/_04_none.py
+++ _02_data_type/_04_none.py
@@ -1,12 +1,10 @@
-# Non type
+# NoneType
 # - 값이 없음을 뜻하는 자료형
 a = None
-print(a,type(a))
+print(a, type(a))
 
 # 변수 값 제거
 s = 'hello'
-print(s,type(s))
+print(s, type(s))
 s = None
-print(s,type(s))
-
-
+print(s, type(s))
--- _02_data_type/_07_set.py
+++ _02_data_type/_07_set.py
@@ -1,47 +1,50 @@
 # set(집합)
 # - 중복 허용 X
-# - 순서 X
+# - 순서 유지 X
 # - 시퀀스 타입 X
-# - 순회(iterable) 가능
+# - 순회(iterable) O
 # - 집합 관련 메서드 제공됨
+# - 기호 : {}
 
 print("--- set ---")
-st = {4, 3, 1, 2, 4, 3, 1, 2,}
+st = {2,1,4,2,1,3,4,2,3,3,3,3,1,1,1,3,2}
 print(st, type(st)) # 중복 제거 확인
-print("--- list -> set 변경 (중복제거) ---")
-lst=[2,3,2,1,4,5,6,3]
-st2 = set(lst) #set 변경
+
+print("--- list -> set 변경 (중복 제거) ---")
+lst = [2,3,1,1,3,4,4]
+st2 = set(lst) # set 변경
+print("st2: ", st2)
 # print(st2[2])
 # TypeError: 'set' object is not subscriptable
 
 # set -> list 변환
 lst2 = list(st2)
-print('lst2[2]: ',lst2[2])
+print("lst2[2]: ", lst2[2])
 
-print("--- tuple -> set 변경 (중복제거) ---")
-tpl=(2,3,2,1,4,5,6,3)
+
+print("--- tuple -> set 변경 (중복 제거) ---")
+tpl = (2,3,1,1,3,4,4)
 st3 = set(tpl)
-print("st3: ",st3)
-
+print("st3: ", st3)
 
 # 요소 추가(add)
 print("--- 요소 추가(add) ---")
-my_nums = {20, 30,40}
+my_nums = {20, 30, 40}
 my_nums.add(10)
 my_nums.add(10) # 중복 제거
 my_nums.add(10) # 중복 제거
-print("my_nums:",my_nums) # {40, 10, 20, 30}
+print("my_nums: ", my_nums) # {40, 10, 20, 30}
 
-#요소 제거(remove)
-print('--- 요소 제거(remove) ---')
+# 요소 제거(remove)
+print("--- 요소 제거(remove) ---")
 my_nums.remove(10)
-print("my_nums:",my_nums)
+print("my_nums: ", my_nums)
 
-#전체 제거(clear)
+# 전체 제거(clear)
 my_nums.clear()
-print("clear 후 my_nums:",my_nums)
+print("clear 후 my_nums: ", my_nums)
 
-#set 순회
+# set 순회
 my_nums = {30, 50, 70, 90}
 
 # my_nums에서 값을 하나 꺼내어 num 변수에 저장(반복)
@@ -58,3 +61,7 @@ print('합집합: ', m.union(n))
 print('교집합: ', m.intersection(n))
 print('차집합: ', m.difference(n))
 print('대칭차집합: ', m.symmetric_difference(n)) # 합집합 - 교집합
+
+
+
+
